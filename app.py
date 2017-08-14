8d2142dc35524f68dce401598368200946ef7ab2
Nick Liu <nl2523@columbia.edu>
2017-08-13 23:36:12 -0400

    Clarifai api call test + adding urllib3[secure] to fix old py2 ssl modules

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
 import requests
 import os
 
 app = Flask(__name__)
+clarifaiApp = ClarifaiApp(api_key='d1df0d143a3a4e4b8ecb696c0ed9dde4')
+
 app.config['DEBUG'] = True
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['FILE_EXTENSIONS'] = ['png', 'jpg', 'pdf', 'jpeg', 'gif']
+
+
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1] in app.config['FILE_EXTENSIONS']
@@ -21,7 +27,18 @@ def uploadFile():
 		fileObj = request.files['file']
 		if fileObj and allowed_file(fileObj.filename):
 			safeFile = secure_filename(fileObj.filename)
-			fileObj.save(os.path.join(app.config['UPLOAD_FOLDER'], safeFile))
+			local_filename = os.path.join(app.config['UPLOAD_FOLDER'], safeFile)
+			
+
+
+			fileObj.save(local_filename)
+
+			model = clarifaiApp.models.get('food-items-v1.0')
+			output = model.predict_by_filename(filename=local_filename)
+
+			for item in output['outputs'][0]['data']['concepts']:
+				print item['name'], ', ', item['value']
+
 			return redirect(url_for('uploaded_file', filename=safeFile))
 
 @app.route('/uploads/<filename>')
